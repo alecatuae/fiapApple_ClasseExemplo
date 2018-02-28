c4e19d89e9c58b28738e0eab7ffd41119209a1fc
Usuário Convidado <Guest@l2602micro61.local>
2018-02-28 20:06:47 -0300

    Initial Commit

---
+++ Classe Exemplo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Classe Exemplo
+//
+//  Created by Usuário Convidado on 28/02/2018.
+//  Copyright © 2018 Agesandro Scarpioni. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
